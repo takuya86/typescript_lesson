bb337eca2d529360de007db39aab7a32432cc997
takuya.kawase <takuya.kawase@nendo.co.jp>
2023-05-20 22:58:33 +0900

    section1

---
+++ index.ts
@@ -0,0 +1,13 @@
+let hello: string = 'hello'
+console.log(hello)
+
+// jsの場合 コメントに残しても合っているのか不明
+function addJs(a, b) {
+  return a +b
+}
+
+// tsの場合何をしているか明確 ドキュメントとして使える
+function addTs(a: number, b: number): number {
+  return a +b
+}
+
