2af2bcbab34f4136a8faf822a4f202bad38fba66
takuya.kawase <takuya.kawase@nendo.co.jp>
2023-05-27 23:01:50 +0900

    section4-50

--- section4/class.ts
+++ section4/class.ts
@@ -6,15 +6,16 @@ class Person {
     this.name = initName
   }
 
-  greeting(this: {name: string}) {
+  greeting(this: Person) {
     console.log(`Hello±! My name is ${this.name}`)
   }
 }
 
+let person2: Person
 const quill = new Person('Quill')
 quill.greeting
 const anotherQuill = {
   name: 'anotherQuill',
-  anotherGreeting: quill.greeting
+  greeting: quill.greeting
 }
-anotherQuill.anotherGreeting()
+anotherQuill.greeting()
