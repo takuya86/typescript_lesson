19d9cf18ab3d26345da7dd3b139e72529ea47851
takuya.kawase <takuya.kawase@nendo.co.jp>
2023-06-14 23:13:14 +0900

    section9-119

---
+++ section9/food-app.ts
@@ -0,0 +1,17 @@
+class Score { }
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler)
+  }
+  clickEventHandler() {
+    this.element.classList.toggle('food--active')
+  }
+}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food')
+  constructor() {
+    this.elements.forEach(element => {
+    })
+  }
+}
+const foods = new Foods()
